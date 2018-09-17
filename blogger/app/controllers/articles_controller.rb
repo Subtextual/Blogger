4423159c9a7f32dc3995189caae4563f1d34b40f
Vivien Holmes <vivien.holmes@bjss.com>
2018-09-17 11:53:19 +0100

    added commenting and tags

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article.id
     end
 
     def new
@@ -42,5 +44,11 @@ class ArticlesController < ApplicationController
         flash.notice = "Article '#{@article.title}' Updated!"
 
         redirect_to article_path(@article)
+    end
+
+    def tag_list
+        @article.tags.collect do |tag|
+            tag.name
+        end.join(", ")
     end
 end
--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -4,5 +4,8 @@ end
 
 Blogger::Application.routes.draw do
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  resources :tags
 end
